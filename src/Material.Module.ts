cb4e3ea0804a561a0ddc04ffa93669665f579b5a
projectharendra <project.harendra@gmail.com>
2024-03-08 22:21:56 +0530

    Initial commit

---
+++ src/Material.Module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import {MatInputModule} from "@angular/material/input"
+@NgModule({
+    exports:[
+        MatInputModule
+    ]
+})
+export class MaterialModule{}
